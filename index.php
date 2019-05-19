88e942b0c106f2f844dbd64b0de38bf8638d2977
dzulfikar68 <dzulfikar.dsdevs@gmail.com>
2019-05-19 17:08:09 +0700

    3 commit

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "todoapp68.database.windows.net";
+    $user = "todoapp68";
+    $pass = "Kaligimber46";
+    $db = "todoapp68";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
